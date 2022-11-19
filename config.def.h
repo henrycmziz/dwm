690a31e354e0386fd669c6fbdd380415aca3f484
Henry Chang <henrycmziz@gmail.com>
2022-11-19 15:55:06 +0000

    add patch dwm-rotatestack-20161021-ab9571b.diff

--- config.def.h
+++ config.def.h
@@ -100,6 +100,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_s,      show,           {0} },
 	{ MODKEY|ShiftMask,             XK_s,      showall,        {0} },
 	{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY,                       XK_e,      rotatestack,    {.i = +1 } },
+	{ MODKEY,                       XK_u,      rotatestack,    {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
